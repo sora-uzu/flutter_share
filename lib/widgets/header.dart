3637939228806202014283d8b6b76b4f99648733
照井宙 <teruichuu@MBA.local>
2021-08-30 20:31:12 +0900

    making AppBars

--- lib/widgets/header.dart
+++ lib/widgets/header.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
-header() {
-  return Text("header");
+header(context, {bool isAppTitle = false, String titleText}) {
+  return AppBar(
+    title: Text(isAppTitle ? 'FlutterShare' : titleText,
+        style: TextStyle(
+          color: Colors.white,
+          fontFamily: isAppTitle ? 'Signatra' : '',
+          fontSize: isAppTitle ? 50 : 22,
+        )),
+    centerTitle: true,
+    backgroundColor: Theme.of(context).accentColor,
+  );
 }

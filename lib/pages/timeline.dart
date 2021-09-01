8b31d88ba7ea3cac48f5080397af47e4f8a16e94
照井宙 <teruichuu@MBA.local>
2021-09-01 22:09:21 +0900

    add getUsers to timeline

--- lib/pages/timeline.dart
+++ lib/pages/timeline.dart
@@ -1,6 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttershare/widgets/header.dart';
 import 'package:fluttershare/widgets/progress.dart';
+
+final usersRef = Firestore.instance.collection('users');
 
 class Timeline extends StatefulWidget {
   @override
@@ -8,6 +11,25 @@ class Timeline extends StatefulWidget {
 }
 
 class _TimelineState extends State<Timeline> {
+  @override
+  void initState() {
+    getUsers();
+    super.initState();
+  }
+
+  getUsers() async {
+    final QuerySnapshot snapshot =
+        await usersRef.where('isAdmin', isEqualTo: false).getDocuments();
+    snapshot.documents.forEach((DocumentSnapshot doc) {
+      print(doc.data);
+    });
+  }
+
+  // getUserById() {
+  //   final String id = ;
+  //   usersRef.document(id).get().then((DocumentSnapshot doc));
+  // }
+
   @override
   Widget build(context) {
     return Scaffold(

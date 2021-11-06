c087a6c477fc3a3a564b843b9b1b98bcddba9f32
照井宙 <teruichuu@MBA.local>
2021-11-06 11:20:27 +0900

    add post screen, see other users profiles

--- lib/pages/activity_feed.dart
+++ lib/pages/activity_feed.dart
@@ -2,6 +2,8 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttershare/pages/home.dart';
+import 'package:fluttershare/pages/post_screen.dart';
+import 'package:fluttershare/pages/profile.dart';
 import 'package:fluttershare/widgets/header.dart';
 import 'package:fluttershare/widgets/progress.dart';
 import 'package:timeago/timeago.dart' as timeago;
@@ -85,10 +87,20 @@ class ActivityFeedItem extends StatelessWidget {
     );
   }
 
-  configureMediaPreview() {
+  showPost(context) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => PostScreen(
+                  postId: postId,
+                  userId: userId,
+                )));
+  }
+
+  configureMediaPreview(context) {
     if (type == 'like' || type == 'comment') {
       mediaPreview = GestureDetector(
-        onTap: () => print('showing post'),
+        onTap: () => showPost(context),
         child: Container(
           height: 50,
           width: 50,
@@ -121,7 +133,7 @@ class ActivityFeedItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    configureMediaPreview();
+    configureMediaPreview(context);
 
     return Padding(
       padding: EdgeInsets.only(bottom: 2),
@@ -129,7 +141,7 @@ class ActivityFeedItem extends StatelessWidget {
         color: Colors.white54,
         child: ListTile(
           title: GestureDetector(
-            onTap: () => print('show profile'),
+            onTap: () => showProfile(context, profileId: userId),
             child: RichText(
               overflow: TextOverflow.ellipsis,
               text: TextSpan(
@@ -160,4 +172,13 @@ class ActivityFeedItem extends StatelessWidget {
       ),
     );
   }
+}
+
+showProfile(BuildContext context, {String profileId}) {
+  Navigator.push(
+      context,
+      MaterialPageRoute(
+          builder: (context) => Profile(
+                profileId: profileId,
+              )));
 }
--- lib/widgets/header.dart
+++ lib/widgets/header.dart
@@ -4,12 +4,15 @@ header(context,
     {bool isAppTitle = false, String titleText, removeBackButton = false}) {
   return AppBar(
     automaticallyImplyLeading: removeBackButton ? false : true,
-    title: Text(isAppTitle ? 'FlutterShare' : titleText,
-        style: TextStyle(
-          color: Colors.white,
-          fontFamily: isAppTitle ? 'Signatra' : '',
-          fontSize: isAppTitle ? 50 : 22,
-        )),
+    title: Text(
+      isAppTitle ? 'FlutterShare' : titleText,
+      style: TextStyle(
+        color: Colors.white,
+        fontFamily: isAppTitle ? 'Signatra' : '',
+        fontSize: isAppTitle ? 50 : 22,
+      ),
+      overflow: TextOverflow.ellipsis,
+    ),
     centerTitle: true,
     backgroundColor: Theme.of(context).accentColor,
   );
